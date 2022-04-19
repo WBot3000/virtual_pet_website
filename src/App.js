edf4a58589621409291474d029955ed4a33f26b1
Belle Roberts <belleroberts13@gmail.com>
2022-04-19 12:29:38 -0400

    Some basic set up
    need to add code to components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className = 'App-title'>Virtual Pet</h1>
+          <Link className = 'Nav-link' to='/'>
+            Home
+          </Link>
+          <Link className = 'Nav-link' to='/myPets'>
+            My Pets
+          </Link>
+          <Link className = 'Nav-link' to='/login'>
+            Login  
+          </Link>
+        </header>
+      <div className='App-body'>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/myPets' element={<PetList/>}/>
+            <Route exact path='/pet/:id' element={<Pet/>}/>
+            <Route exact path='/login' element={<Login/>}/>
+            <Route exact path='/signup' element={<Signup/>}/>
+          </Routes>
+          
+        </div>
+    </div>  
   );
 }
 
