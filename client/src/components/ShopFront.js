ea3b84cd53aeb79a1d3068ad4c3e09765a535de5
vpate2804 <92997920+vpate2804@users.noreply.github.com>
2022-05-13 01:26:37 -0400

    Individual Shop

--- client/src/components/ShopFront.js
+++ client/src/components/ShopFront.js
@@ -2,14 +2,11 @@ import {useState, useEffect} from 'react';
 
 function ShopFront(props) {
     const [shopName, setShopName] = useState("");
-    const [shopQuote, setShopQuote] = useState("Welcome!");
     const [shopInv, setShopInv] = useState([]);
 
     useEffect(() => {
-        setShopName(props.shopName);
-        let quoteIdx = Math.floor(Math.random() * props.shopQuotes.length);
-        setShopQuote(props.shopQuotes[quoteIdx]);
-        setShopInv(props.shopInv);
+        setShopName(props.data.name);
+        setShopInv(props.data.inventory);
     }, []);
 
     function displayInventory() {
@@ -19,7 +16,7 @@ function ShopFront(props) {
         else {
             return <>
                 {shopInv.map(item => {
-                    return <div className="menu_item">
+                    return <div className="menu_item" key={item}>
                         <img src={require('../assets/inv/placeholder.png')} />
                         <p>{item.name}</p>
                         <button>Buy Item</button>
@@ -31,7 +28,6 @@ function ShopFront(props) {
 
     return <>
         <h2>{shopName}</h2>
-        <p>{shopQuote}</p>
         <div id="menu">
             {displayInventory()}
         </div>
---
+++ client/src/pages/ShopPage.js
@@ -0,0 +1,68 @@
+import ShopFront from "../components/ShopFront";
+import axios from 'axios';
+import { useState, useContext, useEffect } from "react";
+import { Navigate, useParams } from 'react-router-dom';
+import CheckUserLoggedIn from "../components/CheckUserLoggedIn";
+
+const ShopPage = () => {
+    const [loading, setLoading] = useState(true);
+    const [checkedUserLoggedIn, setCheckedLogin] = useState(false);
+    const [userIsAuthenticated, setAuthenticated] = useState(false);
+    const [currentUserID, setCurrentUserID] = useState(null);
+    const [shopData, setShopData] = useState(null);
+    const [errorOccured, setError] = useState(false);
+
+    let { id } = useParams();
+    useEffect(() => {
+        async function fetchData() {
+            if (currentUserID) {
+                try {
+                    let { data } = await axios.get(`http://localhost:3001/getShop/${id}`);
+                    setShopData(data);
+                    setLoading(false);
+                } catch (e) {
+                    setError(true);
+                }
+            }
+        }
+        fetchData();
+    }, [id,currentUserID]);
+
+    const onFinishedChecking = (finished) => {
+        setCheckedLogin(true);
+    };
+
+    const onSetAuthenticated = (authenticated) => {
+        setAuthenticated(authenticated)
+    }
+
+    const onSetCurrentUserID = (id) => {
+        setCurrentUserID(id)
+    }
+
+    if (!checkedUserLoggedIn){
+        return <CheckUserLoggedIn onFinished={onFinishedChecking} userIsAuthenticated={onSetAuthenticated} setUserId={onSetCurrentUserID}></CheckUserLoggedIn>
+    }
+    else if (!userIsAuthenticated){
+        return <Navigate to="/"></Navigate>
+    }else if (errorOccured) {
+        return (
+            <div>
+                <Navigate to="/" />
+            </div>
+        );
+    }
+    else if (loading) {
+        return (
+            <div>
+                <h2>Loading....</h2>
+            </div>
+        );
+    } else {
+        return (
+            <ShopFront data={shopData}/>
+        );
+    }
+}
+
+export default ShopPage;
